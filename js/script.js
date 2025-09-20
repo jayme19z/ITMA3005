d5f40503f074c8870b60f540366d616be5a8f475
unknown <kozhagulovaz9@gmail.com>
2025-09-20 14:48:19 +0500

    add: contact-me section, fix: bugs, feat: dark mode

--- js/script.js
+++ js/script.js
@@ -1,24 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("contactForm");
-
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        let name = document.getElementById("name").value.trim();
-        let phone = document.getElementById("phone").value.trim();
-        let request = document.getElementById("request").value.trim();
-
-        if (!name || !phone || !request) {
-            alert("Please fill in all fields.");
-        } else {
-            alert("Form submitted successfully.");
-            this.reset();
-        }
-    });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("contactForm");
     const themeToggle = document.getElementById("theme-toggle");
     const body = document.body;
 
@@ -50,4 +31,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
-
